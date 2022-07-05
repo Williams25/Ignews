f024aaa5315a391a41a7f106fb4111959705f514
Williams25 <william.souza.timao@hotmail.com>
2022-07-05 20:51:14 -0300

    criando header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,24 @@
+import { SignInButton } from "components/SignInButton";
+import Image from "next/image";
+import Link from "next/link";
+import styled from "./header.module.scss";
+
+export const Header = () => {
+  return (
+    <header className={styled.headerContainer}>
+      <div className={styled.headerContent}>
+        <Image src="/images/logo.svg" alt="ig.news" width="300" height="300" />
+        <nav>
+          <Link href="#">
+            <a className={styled.active}>Home</a>
+          </Link>
+          <Link href="#">
+            <a>Posts</a>
+          </Link>
+        </nav>
+
+        <SignInButton />
+      </div>
+    </header>
+  );
+};
