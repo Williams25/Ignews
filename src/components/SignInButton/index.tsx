950a76a75f697639f8f7e8039ab1bcb7b973b5ec
Williams25 <william.souza.timao@hotmail.com>
2022-07-07 20:40:04 -0300

    configurando botão para realizar autenticação e logoof e recuperando nome do usuario ao logar

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,14 +1,29 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
 import styled from "./signInbutton.module.scss";
+import { signIn, useSession, signOut } from "next-auth/react";
 
 export const SignInButton = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
+  const isVerifiSession = Boolean(session && session.user);
+
+  const handleSigninOrSignOut = () => {
+    if (isVerifiSession) {
+      signOut();
+    } else {
+      signIn("github");
+    }
+  };
+
   return (
-    <button type="button" className={styled.signinButton}>
-      <FaGithub color={isUserLoggedIn ? "#04d361" : "#eba417"} />
-      Sign In width Github
-      {isUserLoggedIn && <FiX color="#737380" />}
+    <button
+      type="button"
+      className={styled.signinButton}
+      onClick={handleSigninOrSignOut}
+    >
+      <FaGithub color={isVerifiSession ? "#04d361" : "#eba417"} />
+      {isVerifiSession ? session.user.name : "Sign In width Github"}
+      {isVerifiSession && <FiX color="#737380" />}
     </button>
   );
 };
