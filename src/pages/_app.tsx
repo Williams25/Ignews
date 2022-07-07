32a215553c6dfa54f1b030ee8ab17cd9570943c4
Williams25 <william.souza.timao@hotmail.com>
2022-07-07 20:40:49 -0300

    instanciando provider de autenticação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,17 +2,18 @@ import { Header } from "components/Header";
 import { AppProps } from "next/app";
 import Head from "next/head";
 import "../styles/global.scss";
+import { SessionProvider } from "next-auth/react";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Home | ig.news</title>
       </Head>
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   );
 };
 
