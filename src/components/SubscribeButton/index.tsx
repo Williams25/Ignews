07ae6e61350ba996dc6febb8b03751f652b9bd5e
Williams25 <william.souza.timao@hotmail.com>
2022-07-09 11:29:00 -0300

    implementando o funcionamento do botão de subscribe, realizando redirect para o pagamento

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { useSession, signIn } from "next-auth/react";
+import { api } from "services/api";
+import { getStripeJS } from "services/stripe-js";
 import styled from "./button.module.scss";
 
 export type SubscribeButtonProps = {
@@ -5,8 +8,33 @@ export type SubscribeButtonProps = {
 };
 
 export const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
+  const { data: session } = useSession();
+
+  const handleSubscribe = async () => {
+    if (!session && !session.user) {
+      signIn("github");
+      return;
+    }
+
+    try {
+      const { data: res } = await api.post("subscribe", { price: priceId });
+      const sessionId = res.sessionId;
+
+      const { redirectToCheckout } = await getStripeJS();
+      await redirectToCheckout({
+        sessionId,
+      });
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
   return (
-    <button type="button" className={styled.subscribeButton}>
+    <button
+      type="button"
+      className={styled.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
